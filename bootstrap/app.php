2bf53759ea67bd8b840afa2585c34dfd4669eb05
Justin LI <lijt2007@hotmail.com>
2025-10-01 21:19:45 +0930

    add error page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,7 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-            if (!app()->environment(['local2', 'testing2']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+            if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
                 return Inertia::render('ErrorPage', ['status' => $response->getStatusCode()])
                     ->toResponse($request)
                     ->setStatusCode($response->getStatusCode());
