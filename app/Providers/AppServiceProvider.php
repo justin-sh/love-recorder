1cd4e445d2664c0cc9d0a5dc3522f931d987e4dc
Justin LI <lijt2007@hotmail.com>
2025-10-06 18:15:25 +1030

    refactor boot functions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,8 +7,10 @@ use App\Models\Event;
 use App\Models\User;
 use App\Policies\ChildPolicy;
 use App\Policies\EventPolicy;
+use GuzzleHttp\Cookie\FileCookieJar;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -31,29 +33,53 @@ class AppServiceProvider extends ServiceProvider
     {
         JsonResource::withoutWrapping();
 
+        $this->initGate();
+
+        $this->initDebugInfo();
+
+        $this->initHttpGlobalOptions();
+    }
+
+    private function initGate(): void
+    {
         Gate::policy(Event::class, EventPolicy::class);
         Gate::policy(Child::class, ChildPolicy::class);
 
-        Gate::before(function (User $user, string $ability){
+        Gate::before(function (User $user, string $ability) {
             Log::debug('Gate.before:' . $user);
         });
+    }
 
-         DB::listen(function ($query) {
-        //         // $query->sql; // The raw SQL query
-        //         // $query->bindings; // The query's parameter bindings
-        //         // $query->time; // The execution time of the query in milliseconds
-        //         // $query->connection; // The name of the database connection
+    private function initDebugInfo(): void
+    {
+        DB::listen(function ($query) {
+            //         // $query->sql; // The raw SQL query
+            //         // $query->bindings; // The query's parameter bindings
+            //         // $query->time; // The execution time of the query in milliseconds
+            //         // $query->connection; // The name of the database connection
 
-        //         // Example: Log the query
-                 Log::channel('sql')->info([
-                     'sql' => $query->sql,
-                     'bindings' => $query->bindings,
-                     'time' => $query->time
-                     // 'connection' => $query->connection,
-                 ]);
+            //         // Example: Log the query
+            Log::channel('sql')->info([
+                'sql' => $query->sql,
+                'bindings' => $query->bindings,
+                'time' => $query->time
+                // 'connection' => $query->connection,
+            ]);
 
-        //         // Example: Dump the query to the browser (for debugging)
-        //         // dump($query->sql, $query->bindings, $query->time);
-             });
+            //         // Example: Dump the query to the browser (for debugging)
+            //         // dump($query->sql, $query->bindings, $query->time);
+        });
+
+    }
+
+    private function initHttpGlobalOptions(): void
+    {
+        Http::globalOptions([
+            'debug' => true,
+            'cookies' => true,
+            'headers' => [
+                'user-agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
+            ],
+        ]);
     }
 }
